719e3a86970f4a4df657df47eab6e8f301a8bc02
Clement B <clementb2019@gmail.com>
2023-08-17 03:32:23 +0300

    Now it's even quicker

--- Geometry.py
+++ Geometry.py
@@ -106,121 +106,79 @@ def whereItCouldBe(mic0, mic1, delta):
 
 
 def whereItCouldBe3D(mic0, mic1, delta, res=400, distmin=None, distmax=None):
-    surface = np.empty((0, 3))  # Initialize with an empty 2D array
     if distmin is None:
         distmin = (mic0.dist(mic1) - delta) / 2
 
     if distmax is None:
-        distmax = mic0.dist(mic1)*5
-    for i in np.logspace(np.log10(distmin + (1 / res)), np.log10(distmax), res):
-        circle = find3D(mic0, mic1, delta, i)
-        res_circle = (i/distmax)*(res)
-        u = circle.draw(int(res_circle))
-        surface = np.append(surface, u, axis=0)
+        distmax = mic0.dist(mic1)*10
+
+    i = np.logspace(np.log10(distmin + (1 / res)), np.log10(distmax), res)
+    circle = np.vectorize(find3D)(mic0, mic1, delta, i)
+    res_circle = (i/distmax)*(res)
+    u = np.concatenate([c.draw(int(r)) for c, r in zip(circle, res_circle)])
+    surface = u.reshape(-1, 3)
+
     return surface
 
 
 def reduce(points1, points2, points3, lim=100):
-    print("taille :", len(points1), len(points2), len(points3))
     stop = False
+    stop1=False
+    stop2=False
+    stop3=False
+    perc=0.6
     while not stop:
         limits1 = np.array([np.min(points1, axis=0), np.max(points1, axis=0)])
         limits2 = np.array([np.min(points2, axis=0), np.max(points2, axis=0)])
-        limits3 = np.array([np.min(points3, axis=0), np.max(points3, axis=0)])
+        limits3 = np.array([np.min(points3, axis=0), np.max(points3, axis=0)])        
+        # agrandir la limite de perc%
+        limits1[0] = limits1[0] - perc*np.abs(limits1[0])
+        limits1[1] = limits1[1] + perc*np.abs(limits1[1])
+        limits2[0] = limits2[0] - perc*np.abs(limits2[0])
+        limits2[1] = limits2[1] + perc*np.abs(limits2[1])
+        limits3[0] = limits3[0] - perc*np.abs(limits3[0])
+        limits3[1] = limits3[1] + perc*np.abs(limits3[1])
 
-        # agrandir la limite de 30%
-        limits1[0] = limits1[0] - 0.1*(limits1[1]-limits1[0])
-        limits1[1] = limits1[1] + 0.1*(limits1[1]-limits1[0])
-        limits2[0] = limits2[0] - 0.1*(limits2[1]-limits2[0])
-        limits2[1] = limits2[1] + 0.1*(limits2[1]-limits2[0])
-        limits3[0] = limits3[0] - 0.1*(limits3[1]-limits3[0])
-        limits3[1] = limits3[1] + 0.1*(limits3[1]-limits3[0])
+        #create a mask to filter out the points that are outside the limits
+        mask1 = np.logical_and(
+            points1 >= limits2[0], points1 <= limits2[1]).all(axis=1)
+        mask2 = np.logical_and(
+            points2 >= limits1[0], points2 <= limits1[1]).all(axis=1)
+        mask3 = np.logical_and(
+            points3 >= limits1[0], points3 <= limits1[1]).all(axis=1)
+        mask4 = np.logical_and(
+            points1 >= limits3[0], points1 <= limits3[1]).all(axis=1)
+        mask5 = np.logical_and(
+            points3 >= limits1[0], points3 <= limits1[1]).all(axis=1)
+        mask6 = np.logical_and(
+            points2 >= limits3[0], points2 <= limits3[1]).all(axis=1)
+        
+        old_len1=len(points1)
+        old_len2=len(points2)
+        old_len3=len(points3)
 
-        # create boolean masks for each set of points
-        mask1 = np.logical_and(points1 >= limits2[0], points1 <= limits2[1]).all(axis=1) \
-            & np.logical_and(points1 >= limits3[0], points1 <= limits3[1]).all(axis=1)
-        mask2 = np.logical_and(points2 >= limits1[0], points2 <= limits1[1]).all(axis=1) \
-            & np.logical_and(points2 >= limits3[0], points2 <= limits3[1]).all(axis=1)
-        mask3 = np.logical_and(points3 >= limits1[0], points3 <= limits1[1]).all(axis=1) \
-            & np.logical_and(points3 >= limits2[0], points3 <= limits2[1]).all(axis=1)
+        mask1=np.logical_and(mask1,mask4)
+        mask2=np.logical_and(mask2,mask6)
+        mask3=np.logical_and(mask3,mask5)
 
-        old_points1 = points1.copy()
-        old_points2 = points2.copy()
-        old_points3 = points3.copy()
+        if len(points1[mask1]) > lim: points1 = points1[mask1]
+        else : stop1=True
+        if len(points2[mask2]) > lim: points2 = points2[mask2]
+        else : stop2=True
+        if len(points3[mask3]) > lim: points3 = points3[mask3] 
+        else : stop3=True
 
-        stop1 = False
-        stop2 = False
-        stop3 = False
-        # filter out the points that are outside the limits
-        if len(points1[mask1]) > lim:
-            points1 = points1[mask1]
-        if len(points2[mask2]) > lim:
-            points2 = points2[mask2]
-        if len(points3[mask3]) > lim:
-            points3 = points3[mask3]
-
-        if np.array_equal(old_points1, points1) and np.array_equal(old_points2, points2) and np.array_equal(old_points3, points3):
-            stop = True
-
-        print("taille :", len(points1), len(points2), len(points3))
-    # find the list with the smallest number of points
-    stop2 = False
-    min_len = min(len(points1), len(points2), len(points3))
-    while (not stop2):
-        old_points1 = points1.copy()
-        old_points2 = points2.copy()
-        old_points3 = points3.copy()
-        if len(points1) == min_len:
-            limits2 = np.array(
-                [np.min(points2, axis=0), np.max(points2, axis=0)])
-            limits3 = np.array(
-                [np.min(points3, axis=0), np.max(points3, axis=0)])
-            mask2 = np.logical_and(
-                points2 >= limits3[0], points2 <= limits3[1]).all(axis=1)
-            mask3 = np.logical_and(
-                points3 >= limits2[0], points3 <= limits2[1]).all(axis=1)
-            if len(points2[mask2]) > lim:
-                points2 = points2[mask2]
-            if len(points3[mask3]) > lim:
-                points3 = points3[mask3]
-        elif len(points2) == min_len:
-            limits1 = np.array(
-                [np.min(points1, axis=0), np.max(points1, axis=0)])
-            limits3 = np.array(
-                [np.min(points3, axis=0), np.max(points3, axis=0)])
-            mask1 = np.logical_and(
-                points1 >= limits3[0], points1 <= limits3[1]).all(axis=1)
-            mask3 = np.logical_and(
-                points3 >= limits1[0], points3 <= limits1[1]).all(axis=1)
-            if len(points1[mask1]) > lim:
-                points1 = points1[mask1]
-            if len(points3[mask3]) > lim:
-                points3 = points3[mask3]
-        elif len(points3) == min_len:
-            limits1 = np.array(
-                [np.min(points1, axis=0), np.max(points1, axis=0)])
-            limits2 = np.array(
-                [np.min(points2, axis=0), np.max(points2, axis=0)])
-            mask1 = np.logical_and(
-                points1 >= limits2[0], points1 <= limits2[1]).all(axis=1)
-            mask2 = np.logical_and(
-                points2 >= limits1[0], points2 <= limits1[1]).all(axis=1)
-            if len(points1[mask1]) > lim:
-                points1 = points1[mask1]
-            if len(points2[mask2]) > lim:
-                points2 = points2[mask2]
-        min_len_new = min(len(points1), len(points2), len(points3))
-        if min_len_new != min_len or np.array_equal(old_points1, points1) and np.array_equal(old_points2, points2) and np.array_equal(old_points3, points3):
-            stop2 = True
-            print("stop!!")
+        if old_len1==len(points1) and old_len2==len(points2) and old_len3==len(points3):
+            perc=perc/1.8
+      
+        if (stop1 and stop2) or (stop1 and stop3) or (stop2 and stop3) or perc ==0:
+            stop=True
 
     return points1, points2, points3
 
 
 def find_intersect(points1, points2, points3):
-    print("taille :", len(points1), len(points2), len(points3))
-    if(len(points1)*len(points2)*len(points3) <= 500000):
-        print("this !!!")
+    if(len(points1)*len(points2)*len(points3) <= 100000):
         # Calcul des distances entre les points
         distances = np.zeros((len(points1), len(points2), len(points3)))
         for i, point1 in enumerate(points1):
@@ -238,11 +196,16 @@ def find_intersect(points1, points2, points3):
             [points1, points2, points3], axis=0), axis=0)
         return (x, y, z)
 
+def compute_distance(point1, point2, point3):
+    distance = np.sqrt(np.sum((point1 - point2)**2)) + np.sqrt(np.sum((point2 - point3)**2)) + np.sqrt(np.sum((point3 - point1)**2))
+    i, j, k = np.argmin([point1, point2, point3], axis=0)
+    return i, j, k, distance
+
 
 def WhereItIs(mic0, mic1, mic2, mic3, delta1, delta2, delta3):
-    points1 = whereItCouldBe3D(mic0, mic1, delta1,res=100)
-    points2 = whereItCouldBe3D(mic0, mic2, delta2,res=100)
-    points3 = whereItCouldBe3D(mic0, mic3, delta3,res=100)
+    points1 = whereItCouldBe3D(mic0, mic1, delta1)
+    points2 = whereItCouldBe3D(mic0, mic2, delta2)
+    points3 = whereItCouldBe3D(mic0, mic3, delta3)
     points1, points2, points3 = reduce(points1, points2, points3)
 
     closest_point1 = points1[0, :]
@@ -261,17 +224,13 @@ def WhereItIs(mic0, mic1, mic2, mic3, delta1, delta2, delta3):
     distmax2 = np.linalg.norm(latest_points2-mic0.get_pos())
     distmax3 = np.linalg.norm(latest_points3-mic0.get_pos())
 
-    print("distmin1 : ", distmin1)
-    print("dismax:", distmax1)
-
     points1 = whereItCouldBe3D(
-        mic0, mic1, delta1, res=1000, distmin=distmin1, distmax=distmax1)
+        mic0, mic1, delta1, res=700, distmin=distmin1, distmax=distmax1)
     points2 = whereItCouldBe3D(
-        mic0, mic2, delta2, res=1000, distmin=distmin2, distmax=distmax2)
+        mic0, mic2, delta2, res=700, distmin=distmin2, distmax=distmax2)
     points3 = whereItCouldBe3D(
-        mic0, mic3, delta3, res=1000, distmin=distmin3, distmax=distmax3)
-
-    points1, points2, points3 = reduce(points1, points2, points3, 20)
-    print("taille afeter:", len(points1), len(points2), len(points3))
+        mic0, mic3, delta3, res=700, distmin=distmin3, distmax=distmax3)
+    
+    points1, points2, points3 = reduce(points1, points2, points3, 50)
 
     print("Coordonée estimé : ", find_intersect(points1, points2, points3))
--- README.md
+++ README.md
@@ -4,16 +4,16 @@ To start using the project just do :
 
     pip install -r .\requirements.txt
 
-You can use "Exemple.py" to test the program, just change the coordinates of the fake bee and the fakes mics and it will create audios that correspond to what the mics would have recorded
+You can use "Exemple3D.py" to test the program, just change the coordinates of the fake bee and the fakes mics and it will create audios that correspond to what the mics would have recorded
     
-    x_bee=-480
-    y_bee=-500
-    z_bee=0 #(In developement...)
+    x_bee= 30
+    y_bee= -400
+    z_bee= 500
     
     mic0=Mic(0,0,0)
-    mic1=Mic(50,0,0)
-    mic2=Mic(0,50,0)
-    mic3=Mic(0,0,100)
+    mic1=Mic(300,0,0)
+    mic2=Mic(0,300,0)
+    mic3=Mic(0,0,300)
 
 If you want to add 30dB of noise to the audios, just uncomment all this lines :
     
